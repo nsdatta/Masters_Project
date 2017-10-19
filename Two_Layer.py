c39ec7094545a3085ec92e556cb352b78de6ed83
nsdatta <nsdatta@gmail.com>
2017-10-19 19:41:27 +0530

    Create Two_Layer.py

---
+++ Two_Layer.py
@@ -0,0 +1,53 @@
+import cv2                 # working with, mainly resizing, images
+import numpy as np         # dealing with arrays
+import os                  # dealing with directories
+from random import shuffle # mixing up or currently ordered data that might lead our network astray in training.
+from tqdm import tqdm      # a nice pretty percentage bar for tasks. Thanks to viewer Daniel BA1/4hler for this suggestion
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
+IMG_SIZE = 50
+LR = 1e-3
+MODEL_NAME = 'vehicle-{}-{}.model'.format(LR, '2conv-basic') # just so we remember which saved model is which, sizes must match
+
+#Load train data
+train_data = np.load('train_data.npy')
+
+convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+if os.path.exists('{}.meta'.format(MODEL_NAME)):
+    model.load(MODEL_NAME)
+    print('model loaded!')
+
+# splitting the training and validation data, from train data:
+
+train = train_data[:-300]
+test = train_data[-300:]
+
+# loading the dataset with labels
+X = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y = [i[1] for i in train]
+
+test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+test_y = [i[1] for i in test]
+
+# Now we fit for 3 epochs ?:
+
+model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}), 
+    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
