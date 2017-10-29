40ef463ad925049ff868a58eb29cd5d698d7dd67
nsdatta <nsdatta@gmail.com>
2017-10-29 11:56:27 +0530

    Update confusion_matrix.py

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -1,10 +1,11 @@
+# importing matplot library for plotting function 
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('Agg') # Support non interactive backends
 
-from pandas_ml import ConfusionMatrix
+from pandas_ml import ConfusionMatrix # Confusion matrix 
 import matplotlib.pyplot as plt
 import numpy as np
-import os
+import os # to read the directories, especially to load the model 
 import tensorflow as tf
 import tflearn
 from tqdm import tqdm
@@ -12,14 +13,16 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 
-IMG_SIZE = 50
+IMG_SIZE = 200
 TEST_DIR = '/home/ubuntu/src/datta_ms/Test'
 
+# Loading the test data generated from image process.py
 test_data = np.load('test_data.npy')
 
 LR = 1e-3
 MODEL_NAME = 'vehicle-{}-{}.model'.format(LR, '6conv-basic') 
 
+# labelling the image
 def label_img(img):
    # print img
     word_label = img.split('_')[-2]
@@ -30,7 +33,7 @@ def label_img(img):
     elif word_label == 'car': return 'Car'
     elif word_label == 'Tru': return 'Truck'
 
-
+# Defining the network 
 convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
 
 convnet = conv_2d(convnet, 32, 5, activation='relu')
@@ -59,11 +62,12 @@ convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categori
 
 model = tflearn.DNN(convnet, tensorboard_dir='log')
 
+# Load the trained model 
 if os.path.exists('{}.meta'.format(MODEL_NAME)):
     model.load(MODEL_NAME)
     print('model loaded!')
 
-# labelled test data
+# Labelling for the entire test data, i.e true data
 
 def testlbltrue_data():
     true_data = []
@@ -78,7 +82,9 @@ def testlbltrue_data():
 def softmax(x):
             e_x = np.exp(x - np.max(x))
             return e_x / e_x.sum() 	
-	
+
+# Predicted data for test dataset from the model 
+
 def testlblpred_data():
     pred_data = []
     for num,data in enumerate(test_data[:677]):
@@ -101,6 +107,7 @@ def testlblpred_data():
 y_true = testlbltrue_data()
 print y_true
 
+# Result is pushed to files 
 with open('true.txt','wb') as f:
     np.savetxt(f,y_true,fmt='%s',delimiter=',')
 
@@ -111,7 +118,9 @@ print y_pred
 with open('pred.txt','wb') as f:
     np.savetxt(f,y_pred,fmt='%s',delimiter=',')
 
+# Confusion matrix 
 confusion_matrix = ConfusionMatrix(y_true, y_pred)
 print("Confusion matrix:\n%s" % confusion_matrix)
 
+# Plotting confusion matrix
 confusion_matrix.plot()
